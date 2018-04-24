21bb064d5969bc7e0a0e5c11428f94baf8b8d740
DevExpressExampleBot <fakeEmail>
2018-04-24 22:24:09 +0300

    Source auto update [skip ci]

--- VB/CustomHeaderButtonsExample/My Project/AssemblyInfo.vb
+++ VB/CustomHeaderButtonsExample/My Project/AssemblyInfo.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System.Reflection
+﻿Imports System.Reflection
 Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
@@ -11,7 +10,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyConfiguration("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("CustomHeaderButtonsExample")>
-<Assembly: AssemblyCopyright("Copyright �  2012")>
+<Assembly: AssemblyCopyright("Copyright ©  2012")>
 <Assembly: AssemblyTrademark("")>
 <Assembly: AssemblyCulture("")>
 
